dcd6343aef140610a4ddb5022dfd4466e0410334
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-05-14 14:39:42 +0200

    Add User Model + Controller

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,18 +2,126 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\User;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends Controller
+class UserController extends FOSRestController
 {
     /**
-     * @Route("/users", name="users_list")
+     * @Rest\Get("/users")
+     * @Annotations\QueryParam(name="_sort", nullable=true, description="Sort field.")
+     * @Annotations\QueryParam(name="_order", nullable=true, description="Sort Order.")
+     * @Annotations\QueryParam(name="_start", nullable=true, description="Start.")
+     * @Annotations\QueryParam(name="_end", nullable=true, description="End.")
      */
-    public function listAction(Request $request)
+    public function indexAction(Request $request,ParamFetcherInterface $paramFetcher)
     {
-        return new JsonResponse(array('name' => []));
+        $sortField = $paramFetcher->get('_sort');
+        $sortOrder = $paramFetcher->get('_order');
+        $start = $paramFetcher->get('_start');
+        $end = $paramFetcher->get('_end');
+
+        $query = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAllQuery($sortField,$sortOrder,$start,$end);
+
+        $paginator = new Paginator($query);
+        $totalCount = $paginator->count();
+
+        $restresult = $query->getResult();
+
+        if ($restresult === null) {
+            return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+        }
+
+        $view = $this->view($restresult, 200)
+            ->setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+            ->setHeader('X-Total-Count', $totalCount);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Get("/users/{id}")
+     */
+    public function getAction($id)
+    {
+        $singleresult = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+        if ($singleresult === null) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        }
+        return $singleresult;
+    }
+
+    /**
+     * @Rest\Post("/users")
+     */
+    public function postAction(Request $request)
+    {
+        $data = new User;
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $password = $request->get('password');
+        $is_admin = $request->get('is_admin',false);
+
+        if (empty($name) || empty($email) || empty($password)) {
+            return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        $data->setName($name);
+        $data->setEmail($email);
+        $data->setPassword($password);
+        $data->setIsAdmin($is_admin);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+
+        return new View($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Put("/users/{id}")
+     */
+    public function updateAction($id, Request $request)
+    {
+        $data = new User;
+        $name = $request->get('name');
+
+        $sn = $this->getDoctrine()->getManager();
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+
+        if (empty($user)) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        } elseif (!empty($name)) {
+            $user->setName($name);
+            $sn->flush();
+            return new View("User Updated Successfully", Response::HTTP_OK);
+        } else return new View("User name or role cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
+    }
+
+    /**
+     * @Rest\Delete("/user/{id}")
+     */
+    public function deleteAction($id)
+    {
+        $data = new User;
+        $sn = $this->getDoctrine()->getManager();
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+        if (empty($user)) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        }
+        else {
+            $sn->remove($user);
+            $sn->flush();
+        }
+        return new View("deleted successfully", Response::HTTP_OK);
     }
 }
