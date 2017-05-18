c98922cfc0df9d994d8b21f9285a3244a0a87b30
yasser <yassermohsen101010@gmail.com>
2017-05-18 11:27:14 +0200

    add permissions get, post requests

---
+++ src/AppBundle/Controller/PermissionController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Attendance;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Schedule;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PermissionController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/api/permissions")
+     * @Annotations\QueryParam(name="_sort", nullable=true, description="Sort field.")
+     * @Annotations\QueryParam(name="_order", nullable=true, description="Sort Order.")
+     * @Annotations\QueryParam(name="_start", nullable=true, description="Start.")
+     * @Annotations\QueryParam(name="_end", nullable=true, description="End.")
+     */
+    public function indexAction(Request $request,ParamFetcherInterface $paramFetcher)
+    {
+        $sortField = $paramFetcher->get('_sort');
+        $sortOrder = $paramFetcher->get('_order');
+        $start = $paramFetcher->get('_start');
+        $end = $paramFetcher->get('_end');
+
+        $query = $this->getDoctrine()
+            ->getRepository('AppBundle:Attendance')
+            ->findRequestedPerm($sortField,$sortOrder,$start,$end);
+
+        $paginator = new Paginator($query);
+        $totalCount = $paginator->count();
+
+        $restresult = $query->getResult();
+
+        if (empty($restresult)) {
+            return new View("there are no Requested Permissions right now", Response::HTTP_NOT_FOUND);
+        }
+
+        $view = $this->view($restresult, 200)
+            ->setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+            ->setHeader('X-Total-Count', $totalCount);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Get("/api/permissions/{user_id}")
+     */
+    public function getAction($user_id)
+    {
+        $query = $this->getDoctrine()->getRepository('AppBundle:Attendance')->findUserPerm($user_id);
+
+        $paginator = new Paginator($query);
+        $totalCount = $paginator->count();
+
+        $restresult = $query->getResult();
+
+        if (empty($restresult)) {
+          return new View("You have no permissions", Response::HTTP_NOT_FOUND);
+        }
+        $view = $this->view($restresult, 200)
+            ->setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+            ->setHeader('X-Total-Count', $totalCount);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Post("/api/permissions")
+     */
+    public function postAction(Request $request)
+    {
+        $user_id = $request->get('user_id');
+        $schedule_id = $request->get('schedule_id');
+        //check user existance
+        $user = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->find($user_id);
+        if(empty($user)){
+            return new View("Error in user id", Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        //check schedule existance
+        $schedule = $this->getDoctrine()
+            ->getRepository('AppBundle:Schedule')
+            ->find($schedule_id);
+        if(empty($schedule)){
+            return new View("Error in schedule id", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        //check owner schedule
+        if($schedule->getTrack() != $user->getTrack()){
+            return new View("That is not your track's schedule", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        //check asking permission before 1 day at least
+        $d1 = $schedule->getDayDate();
+        $d2 = new \DateTime('now', new \DateTimeZone('Africa/Cairo'));
+        $interval = $d2->diff($d1);
+        $t = (int)$interval->format("%r%a");
+        if($t <= 0){
+            return new View("You are not allowed to request permission in that day", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        //check if requested permission before
+        $e = $this->getDoctrine()->getManager();
+        $attendance = $e->getRepository('AppBundle:Attendance')
+                          ->findOneBy(
+                              array('user' => $user, 'schedule' => $schedule)
+                          );
+        if(!empty($attendance)){
+            return new View("You have already asked permission for that schedule before", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $attendance = new Attendance();
+        $attendance->setUser($user);
+        $attendance->setSchedule($schedule);
+        $attendance->setRequestPerm(1);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($attendance);
+        $em->flush();
+
+        return new View($attendance, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Delete("/api/permissions/{id}")
+     */
+    // public function deleteAction($id)
+    // {
+    //     $sn = $this->getDoctrine()->getManager();
+    //     $attendance = $sn->getRepository('AppBundle:Attendance')->find($id);
+    //     if (empty($attendance)) {
+    //         return new View("not found", Response::HTTP_NOT_FOUND);
+    //     }
+    //     else {
+    //         $sn->remove($attendance);
+    //         $sn->flush();
+    //     }
+    //     return new View("deleted successfully", Response::HTTP_OK);
+    // }
+}
